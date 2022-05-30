41bc1815bda60a68fd9cf3ad4e099bed018e3f77
Stephen Postma <stephensebastiaanpostma@gmail.com>
2022-05-30 16:35:29 +0200

    toiletcontrollerandview

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,2 +1,35 @@
 class ToiletsController < ApplicationController
+  def index
+    @toilets = Toilet.all
+  end
+
+  def show
+    @toilet = Toilet.find(params[:id])
+  end
+
+  def new
+    @toilet = Toilet.new
+  end
+
+  def create
+    @toilet = Toilet.new(toilet_params)
+    @toilet.save ? (redirect_to toilet_path(@toilet)) : (render :new)
+  end
+
+  def edit
+    @toilet = Toilet.find(params[:id])
+  end
+
+  def update
+    @toilet = Toilet.find(params[:id])
+    @toilet.update(toilet_params)
+
+    redirect_to toilet_path(@toilet)
+  end
+
+  private
+
+  def toilet_params
+    params.require(:toilet).permit(:name, :description, :price, :available, photos: [])
+  end
 end
