ad4a07e12646c1d0359c9f7aa680ac0a65a40375
Grazia Cabras <graziacabras.gcabras@gmail.com>
2022-05-31 17:52:58 +0200

    add booking action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,18 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.toilet = Toilet.find(params[:toilet_id])
+    @booking.save ? (redirect_to toilet_path(@booking.toilet)) : (render :new)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :duration)
   end
 end
