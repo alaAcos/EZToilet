eec735b2c4eded76e0465ede8329327e015f2dbb
Stephen Postma <stephensebastiaanpostma@gmail.com>
2022-06-01 22:16:10 +0200

    added map on show page, fixed map styling, indentation in some pages, other small things

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -14,6 +14,7 @@ class ToiletsController < ApplicationController
   def show
     @toilet = Toilet.find(params[:id])
     @booking = Booking.new
+    @marker = [{ lat: @toilet.latitude, lng: @toilet.longitude, info_window: render_to_string(partial: "info_window", locals: { toilet: @toilet }), image_url: helpers.asset_url("toileticon.png")}]
   end
 
   def new
