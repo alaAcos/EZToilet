45490aebc3b62945eb5646b4e8de1d52ceb7291e
Grazia Cabras <graziacabras.gcabras@gmail.com>
2022-06-29 17:19:20 +0200

    rating system with stars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_112400) do
+ActiveRecord::Schema.define(version: 2022_06_29_131112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
